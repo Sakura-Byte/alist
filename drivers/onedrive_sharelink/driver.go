83f50ce81978e70e999a940fbe222f6ef314b3c3
Sakura-Byte <42319937+Sakura-Byte@users.noreply.github.com>
2023-07-18 05:13:05 +0800

    Update driver.go

--- drivers/onedrive_sharelink/driver.go
+++ drivers/onedrive_sharelink/driver.go
@@ -26,13 +26,6 @@ func (d *OnedriveSharelink) GetAddition() driver.Additional {
 }
 
 func (d *OnedriveSharelink) Init(ctx context.Context) error {
-	//test if d.RodAddress can be connected(websocket)
-	//if not, return error
-	if d.ShareLinkPassword != "" {
-		if err := d.testRod(d.RodAddress); err != nil {
-			return err
-		}
-	}
 	//init err
 	var err error
 	// if there is "-my" in the url, it is NOT a sharepoint link
